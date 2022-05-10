7968bd43444675e214d9209d1856204c32c42f63
Silvère Marie <silvere.marie@external.grtgaz.com>
2022-05-10 17:24:31 +0200

    add tank

--- apps/marsrover/src/app/rover/rover.directive.ts
+++ apps/marsrover/src/app/rover/rover.directive.ts
@@ -14,16 +14,19 @@ export class RoverDirective implements MarsElement {
 
   render(rendererCanvas: ElementRef<HTMLCanvasElement>): void {
     const context = rendererCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
-    const centerX = rendererCanvas.nativeElement.width / 2;
-    const centerY = rendererCanvas.nativeElement.height / 2;
-    const radius = 70;
 
-    context.beginPath();
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = 'green';
-    context.fill();
-    context.lineWidth = 5;
-    context.strokeStyle = '#003300';
-    context.stroke();
+    const base = new Image();
+    base.src = "assets/Hull_01.png";
+    base.onload = function () {
+      context.drawImage(base, 45, 10, base.width, base.height, 0, 0, base.width, base.height);
+    }
+
+
+    const weapon = new Image();
+    weapon.src = "assets/Gun_03_B.png";
+    weapon.onload = function () {
+      context.drawImage(weapon, 0, 0, weapon.width, weapon.height, 50, 110, weapon.width, weapon.height);
+    }
+
   }
 }
